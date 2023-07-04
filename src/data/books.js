c025e326b582dc7ce02ab9168a7d5d92d1c3e68c
eavelasquez <eavc.30@gmail.com>
2023-07-04 14:25:02 -0500

    feat: add book data

---
+++ src/data/books.js
@@ -0,0 +1,30 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: Manual de desarrollo ágil de software',
+		image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		opinion: '',
+		author: 'Robert C. Martin'
+	},
+	{
+		id: 'the-clean-coder',
+		title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+		image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+		opinion: '',
+		author: 'Robert C. Martin'
+	},
+	{
+		id: 'javascript-the-good-parts',
+		title: 'JavaScript: The Good Parts',
+		image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+		opinion: '',
+		author: 'Douglas Crockford'
+	},
+	{
+		id: 'eloquent-javascript',
+		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+		image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+		opinion: '',
+		author: 'Marijn Haverbeke'
+	}
+]
